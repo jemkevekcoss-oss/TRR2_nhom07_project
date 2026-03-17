fabf5344e77810d9bcc5a74fe56a05834b85fc04
Nhat Anh Nguyen Van <jemkevekcoss@gmail.com>
2026-03-17 17:09:26 +0700

    Update some documents

---
+++ TRR2_nhom07_project/constValue.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream> 
+
+
+enum ProgramState {
+	MENU, //0
+	PLAYING, //1
+	PAUSE, //2
+	FINISHED //3
+};
+
+
+
+inline int screenWidth = 1900;
+inline int screenHeight = 1000;
+
+
+
+
--- TRR2_nhom07_project/main.cpp
+++ TRR2_nhom07_project/main.cpp
@@ -1,10 +1,36 @@
-#include "mylib.h"
+#include "constValue.h"
+#include "raylib.h"
 
 using namespace std;
 
+
+/*
+Game state
+
+
+*/
+
 int main() {
+    // Khởi tạo cửa sổ 1 lần duy nhất
+    InitWindow(screenWidth, screenHeight, "Graph Algorithm Visualizer");
+    SetTargetFPS(60);
 
-	Animal a;
-	a.makeNoise();
+    //Visualizer app; // Khởi tạo toàn bộ game/app ở đây
 
+
+    // Handle Input -> Update Program -> Render/Drawing 
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        
+        //app.Update(); // Xử lý input và logic thuật toán
+        //app.Draw();   // Chỉ lo việc vẽ
+
+
+
+        EndDrawing();
+    }
+
+    //app.Cleanup(); // Giải phóng bộ nhớ, âm thanh
+    CloseWindow();
+    return 0;
 }
